01bbd892ec9cb03cdf4de35064c9a9377ad14210
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2022-10-30 13:52:53 -0500

    commented out if else statements and added for loop

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Lets Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("lets study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript");
+} else {
+    console.log('Please try again');
+}
+*/
